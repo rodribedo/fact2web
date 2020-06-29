e34be3ee8159f14a77bd78ba4a61534f483e51b3
rodribedo <rodribedo@gmail.com>
2020-06-29 15:06:36 -0500

    Titulo descriptivo

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if (!isset($_REQUEST['pag']))
 	 
  
 	<div id="divContenedor">
-<!--    <div id="divEncabezado">
+<!--    <div id="divEncabezado de la aplicacion Fac2web">
 	<___php include "Vista/encabezado.php"?>
 
     <a  align="right" class="btn btn-success btnterapia" href="index.php?pag=Vista/registro">Registrese aqui</a>
